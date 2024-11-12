cafd2e3f5646a479b3efeb7c33facf5124a9cd03
MonroeGollaher <mgollaher92@gmail.com>
2024-11-12 16:31:18 -0700

    add button component and other basic stuff

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./index.module.css";
+import classNames from "classnames";
+
+export const Button = ({
+  className,
+  onClick,
+  buttonText,
+  type = "button",
+  disabled = false,
+}) => {
+  return (
+    <button
+      className={classNames(className, styles.component)}
+      onClick={onclick}
+      type={type}
+      disabled={disabled}
+    >
+      {buttonText}
+    </button>
+  );
+};
